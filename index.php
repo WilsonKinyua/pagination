2ce2b098b65e570b0a9405496bf99d99fcfcf644
wilson <wilsonkinyuam@gmail.com>
2020-09-21 01:36:55 +0300

    final

--- index.php
+++ index.php
@@ -1,43 +1,189 @@
 <?php
-        // connect to database
-        $con            = mysqli_connect('localhost','root','');
-        mysqli_select_db($con, 'pagination');
-
-        // define how many results you want per page
-        $results_per_page = 8;
-
-        // find out the number of results stored in database
-        $sql            ='SELECT * FROM alphabet';
-        $result         = mysqli_query($con, $sql);
-        $num_rows       = mysqli_num_rows($result);
-
-        
-        // determine number of total pages available
-        $number_of_pages = ceil($num_rows/$results_per_page);
-
-        // determine which page number visitor is currently on
-        if (!isset($_GET['page'])) {
-            $page = 1;
-
-            } else {
-                $page = $_GET['page'];
-        }
-
-        // determine the sql LIMIT starting number for the results on the displaying page
-        $this_page_first_result = ($page - 1) * $results_per_page;
-
-        // retrieve selected results from database and display them on page
-        $sql='SELECT * FROM alphabet LIMIT ' . $this_page_first_result . ',' .  $results_per_page;
-        $result = mysqli_query($con, $sql);
-
-        while($row = mysqli_fetch_array($result)) {
-        echo $row['id'] . ' ' . $row['alphabet']. '<br>';
-        }
-
-        // display the links to the pages
-        for ($page=1; $page<=$number_of_pages; $page++) {
-            echo '<a href="index.php?page=' . $page . '">' . $page . '</a> ';
-        }
-
-
+$connect = mysqli_connect("localhost", "root", "", "users_test");
+$query = "SELECT * FROM tbl_employee";
+$result = mysqli_query($connect, $query);
 ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Remembering</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <br /><br />
+    <div class="container" style="width:700px;">
+        <h3 align="center">Bootstrap Modal </h3>
+        <br />
+        <div class="table-responsiver">
+            <div align="left">
+                <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal" class="btn btn-warning">Add</button>
+            </div>
+            <br />
+            <div class="employee_table">
+            <table class="table table-bordered">
+                <tr>
+                    <th width="70%">Employee Name</th>
+                    <th width="30%">Edit</th>
+                    <th>View</th>
+                </tr>
+                <?php
+                while ($row = mysqli_fetch_array($result)) {
+                ?>
+                    <tr>
+                        <td><?php echo $row["name"]; ?></td>
+                        <td><input type="button" name="edit" value="Edit" id="<?php echo $row["id"]; ?>" class="btn btn-info btn-xs edit_data" /></td>  
+                        <td><input type="button" class="btn btn-info btn-xs view_data" id="<?php echo $row['id']; ?>" name="view" value="view"></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </table>
+            </div>
+        </div>
+    </div>
+    <!-- Modal insert into the database -->
+    <div id="add_data_Modal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">PHP Ajax Insert Data in MySQL By Using Bootstrap Modal</h4>
+                </div>
+                <div class="modal-body">
+                    <form method="post" id="insert_form">
+                        <label>Enter Employee Name</label>
+                        <input type="text" name="name" id="name" class="form-control" />
+                        <br />
+                        <label>Enter Employee Address</label>
+                        <textarea name="address" id="address" class="form-control"></textarea>
+                        <br />
+                        <label>Select Gender</label>
+                        <select name="gender" id="gender" class="form-control">
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                        <br />
+                        <label>Enter Designation</label>
+                        <input type="text" name="designation" id="designation" class="form-control" />
+                        <br />
+                        <label>Enter Age</label>
+                        <input type="text" name="age" id="age" class="form-control" />
+                        <br />
+                        <input type="submit" name="insert" id="insert" value="Insert" class="btn btn-success" />
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    
+    <!-- modal view data -->
+    <div id="dataModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Employee Details</h4>
+                </div>
+                <div class="modal-body" id="employee_detail">
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+
+            $('#add').click(function(){  
+                $('#insert').val("Insert");  
+                $('#insert_form')[0].reset();  
+            });  
+
+            $(document).on('click', '.edit_data', function(){  
+                var employee_id = $(this).attr("id");  
+                $.ajax({  
+                        url:"fetch.php",  
+                        method:"POST",  
+                        data:{employee_id:employee_id},  
+                        dataType:"json",  
+                        success:function(data){  
+                            $('#name').val(data.name);  
+                            $('#address').val(data.address);  
+                            $('#gender').val(data.gender);  
+                            $('#designation').val(data.designation);  
+                            $('#age').val(data.age);  
+                            $('#employee_id').val(data.id);  
+                            $('#insert').val("Update");  
+                            $('#add_data_Modal').modal('show');  
+                        }  
+                });  
+            }); 
+
+
+            // Insert data into the database using AJAX
+            $("#insert_form").on("submit",function(event){
+                event.preventDefault();
+                if($('#name').val() == ""){  
+
+                    alert("Name is required"); 
+
+                }else if($('#address').val() == '') {  
+
+                    alert("Address is required");  
+
+                }else if($('#designation').val() == ''){ 
+
+                    alert("Designation is required");  
+                } else {
+                    $.ajax({
+                        url:"insert.php",
+                        method:"POST",
+                        data:$('#insert_form').serialize(),
+                        beforeSend:function() {
+                            $('#insert').val("Inserting");  
+                        },
+                        success:function(data){
+                            $('#insert_form')[0].reset();  
+                            $('#add_data_Modal').modal('hide');  
+                            $('#employee_table').html(data);
+                        }
+                    });
+                }
+            });
+
+            //  View data from the database using a modal
+            $(".view_data").click(function() {
+                var employee_id = $(this).attr("id");
+                $.ajax({
+                    url: "select.php",
+                    method: "post",
+                    data: {
+                        employee_id: employee_id
+                    },
+                    success: function(data) {
+                        $("#employee_detail").html(data);
+                        $("#dataModal").modal("show");
+                    }
+                });
+            });
+
+
+        });
+    </script>
+</body>
+
+</html>
